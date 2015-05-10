e2898572147001a72c6545be99f3736bcfe0611a
tormod17 <tormodsmith@gmail.com>
2015-05-10 12:38:41 +0100

    added progress bar and another question

--- js/app.js
+++ js/app.js
@@ -5,12 +5,14 @@ $(document).ready(function(){
 	var tOf= 1;
 	var booleanAnswer;
 	var answerNumber;
+	var counter=0;
 	
 	 		  	
 		$('#restartQuiz').click(function() {
 				currentQuestion =0;
 				questionNumber= 1;
 				answerNumber=0;
+				counter=0;
 				$('.fistWrapper img').remove();
 				$('#questionBar').text('Q '+ questionNumber + ': ' + qna[currentQuestion].question);
 			for ( var i=1; i < 4; i++) {
@@ -29,6 +31,8 @@ $(document).ready(function(){
 
 		$('#popupContinueButton').click(function(){
 			$('#Popup').hide(300);
+			$(this).removeClass(".rightAnswer");
+			$(this).removeClass(".wrongAnswer");
 			
 		});	
 
@@ -44,13 +48,15 @@ $(document).ready(function(){
 				answerNumber = $(this).attr('id');
 				answerNumber=parseInt(answerNumber[3])
 				console.log (answerNumber);
+				counter+= 16.666;
+				drawszlider(100,counter);
 				
 				if ( qna[currentQuestion].answers[answerNumber-1][tOf] == true ) {
 						
 					
 						$('.responseImg').addClass("rightAnswer");
 						$('.wrongTxt').hide();
-						$('.rightTxt').show(500);
+						$('.rightTxt').show(300);
 						$('#Popup').show(300);///css({"display":'inline-block'});
 						$('.fistWrapper').append('<img src="http://i1378.photobucket.com/albums/ah112/Tormod_Smith/fist_zpsmhwxvwuk.jpg">');
 						
@@ -58,8 +64,8 @@ $(document).ready(function(){
 
 
 						$('.responseImg').addClass("wrongAnswer");
-						$('.rightTxt').hide();						
-						$('.wrongTxt').show(500);
+						$('.rightTxt').hide();	
+						$('.wrongTxt').show(300);
 						$('#Popup').show(300);///css({"display":'inline-block'});
 						
 			
@@ -72,7 +78,14 @@ $(document).ready(function(){
 						$('#ans' + i).text(qna[currentQuestion].answers[i-1][0]);
 				};	
 	 	
-	 }		
+	 }	
+
+
+	function drawszlider(ossz, meik){
+	    var szazalek=Math.round((meik*100)/ossz);
+	    document.getElementById("szliderbar").style.width=szazalek+'%';
+	    document.getElementById("szazalek").innerHTML=szazalek+'%';
+	}	
 	
 
 
@@ -98,7 +111,8 @@ answers: [['The One',true], ['The 300 and Chuck',false],['The Invincibles',false
 {
 question: 'When Chuck Norris turned 18, what did his parents do?',
 answers: [['Buy him a car',false],['force him to get a job',false],['They moved out',true]]
-}
-];
-
-
+},
+{
+ question :"Why does Chuck Norris' calendar go from March 31st to April 2nd?",
+ answers : [['Chuck can\'t read a calendar',false],['he follows his own calendar',false],['No one fools Chuck Norris',true]]
+}];
